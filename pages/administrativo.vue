4b6619e825d1737d9b30251f3245b1b3cabb9291
MaicoLopez <miguel.lopez@grupovizer.com>
2024-10-02 23:44:25 -0600

    Parte lo de jaqui

---
+++ pages/administrativo.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <div
+      class="min-h-screen flex items-center justify-center bg-black bg-opacity-40 p-6"
+    >
+      <div class="bg-white rounded-lg shadow-lg p-10 max-w-2xl w-full">
+        <AdministrativoAdmin />
+      </div>
+    </div>
+  </section>
+</template>
